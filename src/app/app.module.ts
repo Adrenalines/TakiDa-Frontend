dbfffc683672b2dc84d2c8c9b89b1f7cce3e4f0b
eevegorov <eevegorov@yandex.ru>
2020-02-14 01:00:35 +0300

    Add slider (slick)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/slider/slider.component.ts
@@ -0,0 +1,36 @@
+import { AfterContentInit, AfterViewInit, Component, OnInit } from '@angular/core';
+import * as jQuery from 'jquery';
+import 'slick-carousel';
+import * as slidesStore from './slidesStore.json';
+
+interface Slide {
+  id: string;
+  title: string;
+  alt: string;
+  src: string;
+}
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.less']
+})
+export class SliderComponent implements OnInit, AfterViewInit {
+  slidesStore: Slide[] = (slidesStore as any).default;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    jQuery('.sl').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      dots: true,
+      fade: true
+    });
+  }
+
+}
