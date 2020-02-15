5ff2d802b24fb6ea780d7e216f9bba3def5855bd
eevegorov <eevegorov@yandex.ru>
2020-02-16 02:35:44 +0300

    Decompose styles and fix header

--- src/app/item/item.module.ts
+++ src/app/item/item.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { DeliveryInfoComponent } from './delivery-info/delivery-info.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { ItemComponent } from './item/item.component';
 
 
 
 @NgModule({
-  declarations: [ProductComponent, DeliveryInfoComponent, BreadcrumbsComponent],
+  declarations: [ProductComponent, DeliveryInfoComponent, BreadcrumbsComponent, ItemComponent],
   imports: [
     CommonModule
   ]
--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -4,11 +4,12 @@ import { BasketComponent } from './basket/basket.component';
 import { OrderTypeComponent } from './order-type/order-type.component';
 import { FastOrderComponent } from './fast-order/fast-order.component';
 import { DetailedOrderComponent } from './detailed-order/detailed-order.component';
+import { OrderComponent } from './order/order.component';
 
 
 
 @NgModule({
-  declarations: [BasketComponent, OrderTypeComponent, FastOrderComponent, DetailedOrderComponent],
+  declarations: [BasketComponent, OrderTypeComponent, FastOrderComponent, DetailedOrderComponent, OrderComponent],
   imports: [
     CommonModule
   ]
